9a422de51cb3bd26b909b9342ccee991e758b7dd
ger0 <mrgero155@gmail.com>
2024-04-02 23:57:35 +0200

    unfinished

--- glsl/compute_vertices.glsl
+++ glsl/compute_vertices.glsl
@@ -1,25 +1,55 @@
 #version 460
 
 layout (local_size_x = 8, local_size_y = 8) in;
+
 layout (std140, binding = 0) uniform config {
-    ivec2 hmap_dims;
-    uint vert_buff_size;
+    ivec2       hmap_dims;
+    vec3        sky_color;
+    unsigned    clip_range;
 };
-struct Vertex {
+
+layout (std140, binding = 1) uniform camera {
+    mat4 perspective;
+    mat4 view;
+
+    vec3 dir;
     vec3 pos;
-    vec3 norm;
 };
-layout (std140, binding = 1) buffer Vertex_buffer {
-    Vertex verts[];
-};
+
 layout (rgba32f, binding = 2) uniform readonly image2D heightmap;
+
+layout (binding = 3, rgba32f) uniform image2D out_tex;
+
+vec4 raymarch(vec3 origin, vec3 direction) {
+    const int max_steps = 100;
+    const float max_distance = 100.0;
+    float epsilon = 0.01;
+
+    float total_distance = 0.0;
+    for (int i = 0; i < max_steps; ++i) {
+        vec3 sample_pos = origin + direction * total_distance;
+        float height = texture(heightmap, sample_pos.xy / hmap_dims).r * 4.0f;
+        if (sample_pos.z < height) {
+            return vec4(1,1,1,1);
+        }
+        total_distance += epsilon;
+        if (total_distance >= max_distance) {
+            // Reached maximum distance without hitting surface, return invalid value
+            return vec4(sky_color, 1.0f);
+        }
+    }
+    // Did not hit surface within maximum steps, return invalid value
+    return vec4(sky_color, 1.0f);
+}
 
 void main() {
     uint idx = gl_LocalInvocationID.x + gl_LocalInvocationID.y * hmap_dims.x;
+    ivec2 pixel = ivec2(gl_GlobalInvocationID.xy);
+    vec2 uv = vec2(pixel * vec2(2.0)) / vec2(gl_NumWorkGroups.xy * gl_WorkGroupSize.xy);
 
-    // check for out-of-bounds access
-    if (idx >= verts.length()) {
-        return;
+    vec4 pos = vec4(camera.pos + camera.dir, 1.f) * perspective * view;
+    for (unsigned z = 1; z < clip_range; z++) {
+        pos 
     }
 
     float x = float(gl_LocalInvocationID.x) / (hmap_dims.x - 1.0f);
@@ -27,10 +57,7 @@ void main() {
     float z = float(gl_LocalInvocationID.y) / (hmap_dims.y - 1.0f);
     float world_y = height * 4.0f;
 
-    Vertex vert;
-    vert.pos    = vec3(x, world_y, z);  // 1.0f for w component (homogeneous clip space)
-    vert.norm   = 
-    // create vert.norm
-
-    verts[idx] = vert;
+    vec4 color = vec4(1, 1, 1, 1);
+    ivec2 store_pos = ivec2(gl_GlobalInvocationID.xy);
+    imageStore(out_tex, store_pos, color);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -26,12 +26,12 @@ constexpr auto simplex_noise_file = "../glsl/simplex_noise.glsl";
 
 // ----------------------
 
-float delta_time = 0.f;
-float last_frame = 0.f;
+static float delta_time = 0.f;
+static float last_frame = 0.f;
 
 // mouse position
-glm::vec2 mouse_last;
-struct Camera {
+static glm::vec2 mouse_last;
+static struct Camera {
 	glm::vec3 pos       = glm::vec3(0.f, 3.f, 3.f);
 	glm::vec3 target    = glm::vec3(0.f, 3.f, 0.f);
 	glm::vec3 dir       = glm::normalize(pos - target);
@@ -46,7 +46,6 @@ struct Camera {
 	float yaw   = -90.f;
 	float pitch = 0.f;
 } camera;
-
 
 void mouse_callback(GLFWwindow *window, double xpos, double ypos) {
 	float xoffset = xpos - mouse_last.x;
@@ -109,14 +108,6 @@ int main(int argc, char* argv[]) {
             destroy_window
         );
 
-	glEnable(GL_DEPTH_TEST);
-	glDepthFunc(GL_LESS);
-
-	glFrontFace(GL_CW);
-	glEnable(GL_BLEND);  
-
-	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-
 	glfwSetInputMode(window.get(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 	glfwSetCursorPosCallback(window.get(), mouse_callback);
 	glfwSetKeyCallback(window.get(), key_callback);
@@ -154,111 +145,67 @@ int main(int argc, char* argv[]) {
     glGenFramebuffers(1, &framebuffer);
     defer { glDeleteFramebuffers(1, &framebuffer); };
 
-    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
-    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, noise_texture, 0);
-    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
-        return EXIT_FAILURE;
-    }    
-    glBindFramebuffer(GL_FRAMEBUFFER, 0);
-
-    // ---------- vertex generation ---------------
-    GLuint ubo_compute_config;
-    glGenBuffers(1, &ubo_compute_config);
-    defer { glDeleteBuffers(1, &ubo_compute_config); };
+    // ---------- output framebuffer  ---------------
+    compute_verts.use();
+    GLuint ubo_config, ubo_camera;
+    glGenBuffers(1, &ubo_config);
+    defer { glDeleteBuffers(1, &ubo_config); };
+    glGenBuffers(1, &ubo_camera);
+    defer { glDeleteBuffers(1, &ubo_camera); };
 
     constexpr u32 group_size = 8;
     constexpr u32 chunk_size = 8;
     constexpr u32 total_size = group_size * chunk_size;
 
     // configuration
-    struct Compute_verts_config {
+    struct Compute_config {
         glm::vec2 dims = glm::ivec2(total_size, total_size);
-        glm::uint vert_buff_size = total_size;
-    } verts_config;
+        glm::vec3 sky_color = glm::vec3(0.45, 0.716, 0.914);
+        float clip_range = Z_FAR;
+    } comput_conf;
 
-    glBindBuffer(GL_UNIFORM_BUFFER, ubo_compute_config);
-    glBufferData(GL_UNIFORM_BUFFER, sizeof(verts_config), &verts_config, GL_STATIC_DRAW);
-    compute_verts.ub_bind((GLchar*)"config", ubo_compute_config);
+    struct Compute_camera {
+        glm::mat4 perspective;
+        glm::mat4 view;
 
-    GLuint buff_compute_verts;
-    glGenBuffers(1, &buff_compute_verts);
-    defer { glDeleteBuffers(1, &buff_compute_verts); };
+        glm::vec3 dir;
+        glm::vec3 pos;
+    } comput_cam;
 
-    struct Vert {
-        glm::vec3 position;
-        glm::vec3 normal;
-    };
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_config);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(comput_conf), &comput_conf, GL_STATIC_DRAW);
+    compute_verts.ub_bind((GLchar*)"config", ubo_config);
 
-    compute_verts.use();
-    glBindBuffer(GL_SHADER_STORAGE_BUFFER, buff_compute_verts);
-    glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(Vert) * total_size, nullptr, GL_STATIC_DRAW);
-    glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, buff_compute_verts);
+    glBindBuffer(GL_UNIFORM_BUFFER, ubo_camera);
+    glBufferData(GL_UNIFORM_BUFFER, sizeof(comput_cam), &comput_cam, GL_STATIC_DRAW);
+    compute_verts.ub_bind((GLchar*)"config", ubo_camera);
+
+	comput_cam.view = glm::lookAt(camera.pos, camera.pos + camera.target, camera.up);
+	comput_cam.perspective = glm::perspective(glm::radians(FOV), ASPECT_RATIO, Z_NEAR, Z_FAR);
 
     glBindTexture(GL_TEXTURE_2D, noise_texture);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, WINDOW_W, WINDOW_H, 0, GL_RGBA, GL_FLOAT, nullptr);
     glBindImageTexture(2, noise_texture, 0, GL_FALSE, 0, GL_READ_ONLY, GL_RGBA32F);
 
-    glDispatchCompute(chunk_size, chunk_size, 1);
+    GLuint output_texture;
+    glGenTextures(1, &output_texture);
+    defer { glDeleteTextures(1, &output_texture); };
 
-    // ---------- rendering ----------
-    shader.use();
+    glBindTexture(GL_TEXTURE_2D, output_texture);
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA32F, WINDOW_W, WINDOW_H, 0, GL_RGBA, GL_FLOAT, nullptr);
+    glBindImageTexture(3, output_texture, 0, GL_FALSE, 0, GL_WRITE_ONLY, GL_RGBA32F);
 
-    constexpr glm::vec3 SKY_COLOR(0.45, 0.716, 0.914);
-    constexpr auto  SUN_DIRECTION = glm::vec3(0, 1, -0.12);
-    constexpr float FOG_DENSITY   = 0.0036;
-    constexpr float FOG_GRADIENT  = 8.0;
+    glBindFramebuffer(GL_FRAMEBUFFER, framebuffer);
+    glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, output_texture, 0);
+    if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+        LOG_ERR("FRAMEBUFFER INCOMPLETE!");
+        return EXIT_FAILURE;
+    }    
+    glBindFramebuffer(GL_FRAMEBUFFER, 0);
 
-	glClearColor(SKY_COLOR.r, SKY_COLOR.g, SKY_COLOR.b, 1.f);
-	glUniform3fvARB(shader.u("skyColor"), 1, glm::value_ptr(SKY_COLOR));
-	glUniform1f(shader.u("far"), Z_FAR);
-	glUniform1f(shader.u("near"), Z_NEAR);
-	glUniform3fv(shader.u("sunDir"), 1, glm::value_ptr(
-				glm::normalize(SUN_DIRECTION)));
-	glUniform1f(shader.u("density"),  FOG_DENSITY);
-	glUniform1f(shader.u("gradient"), FOG_GRADIENT);
+    glDispatchCompute(WINDOW_W / 8, WINDOW_H / 8, 1);
 
 	float near_plane = 1.0f, far_plane = 7.f;
-	static glm::mat4 light_proj = glm::ortho(-10.0f, 10.0f, -10.0f, 10.0f, near_plane, far_plane);
-	static glm::mat4 light_view = glm::lookAt(glm::vec3(-2.0f, 4.0f, -1.0f), 
-                                  	  glm::vec3( 0.0f, 0.0f,  0.0f), 
-                                  	  glm::vec3( 0.0f, 1.0f,  0.0f));
-	static glm::mat4 light_space_mat = light_proj * light_view; 
-	glUniformMatrix4fv(shader.u("lightSpaceMatrix"), 1, GL_FALSE, glm::value_ptr(light_space_mat));
-
-    GLuint vao;
-    glGenVertexArrays(1, &vao);
-
-    auto M = glm::mat4(1.f);
-	auto V = glm::lookAt(camera.pos, camera.pos + camera.target, camera.up);
-	auto P = glm::perspective(glm::radians(FOV), ASPECT_RATIO, Z_NEAR, Z_FAR);
-
-	glUniformMatrix4fv(shader.u("M"), 1, false, glm::value_ptr(M));
-	glUniform3fvARB(shader.u("camera_pos"), 1, glm::value_ptr(camera.pos));
-	glUniformMatrix4fv(shader.u("V"), 1, false, glm::value_ptr(V));
-	glUniformMatrix4fv(shader.u("P"), 1, false, glm::value_ptr(P));
-
-	glBindVertexArray(vao);
-	// binding vertices generated earlier
-	glBindBuffer(GL_ARRAY_BUFFER, buff_compute_verts);
-
-	uint count = 3;
-	// passing position vector to vao
-	glVertexAttribPointer(shader.a("vertex"), count, GL_FLOAT, GL_FALSE,
-        	sizeof(Vert), (GLvoid*)offsetof(Vert, position));
-	glEnableVertexAttribArray(shader.a("vertex"));
-
-	/* // passing normal vector to vao
-	glVertexAttribPointer(shader.a("normal"), count, GL_FLOAT, GL_FALSE,
-        	sizeof(Vert), (GLvoid*)offsetof(Vert, normal));
-	glEnableVertexAttribArray(shader.a("normal")); */
-
-	/* glVertexAttribPointer(sp->a("color"), count + 1, GL_FLOAT, GL_FALSE,
-        	sizeof(Vertex), (GLvoid*)offsetof(Vert, color));
-	glEnableVertexAttribArray(sp->a("color")); */
-
-	glDrawArrays(GL_TRIANGLES, 0, total_size);
-
-	// glBindVertexArray(0);
 
     while (!glfwWindowShouldClose(window.get())) {
         float current_frame = glfwGetTime();
@@ -266,22 +213,14 @@ int main(int argc, char* argv[]) {
         last_frame = current_frame;
 
         glfwPollEvents();
-        // 1 rendering pass
-        /* {
-           glViewport(0, 0, SHADOW_W, SHADOW_H);
-           configureShaderAndMatrices(spshad.get());
-           wglBindFramebuffer(GL_FRAMEBUFFER, depthMapFBO);
-           glClear(GL_DEPTH_BUFFER_BIT);
-           glBindFramebuffer(GL_FRAMEBUFFER, 0);
-        } */
 
-        // 2 rendering pass
+        glDispatchCompute(WINDOW_W / 8, WINDOW_H / 8, 1);
+
         glViewport(0, 0, WINDOW_W, WINDOW_H);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-	    glDrawArrays(GL_TRIANGLES, 0, total_size);
-        /* glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
+        glBindFramebuffer(GL_READ_FRAMEBUFFER, framebuffer);
         glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-        glBlitFramebuffer(0, 0, WINDOW_W, WINDOW_H, 0, 0, WINDOW_W, WINDOW_H, GL_COLOR_BUFFER_BIT, GL_NEAREST); */
+        glBlitFramebuffer(0, 0, WINDOW_W, WINDOW_H, 0, 0, WINDOW_W, WINDOW_H, GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
         // imgui
         ImGui_ImplOpenGL3_NewFrame();
